4bb8f4d5edf0d3f1b1b36180363b57decaaf2b6f
Patrick Jansson <patrick.jansson@valuemotive.com>
2024-01-01 17:04:41 +0200

    Add keybinds and none-ls

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("options")
+require("keybinds")
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
---
+++ lua/keybinds.lua
@@ -0,0 +1,6 @@
+-- Clear hightlight on Esc
+vim.keymap.set('n', '<Esc>', '<Esc>:noh<CR>')
+
+-- Center on up & down
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
