383921973b94954a1539603e2f2215b9908994ce
Patrick Jansson <patrick.jansson@outlook.com>
2024-12-12 13:35:58 +0200

    Tab options

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,9 @@ vim.opt.laststatus = 3
 vim.opt.cmdheight = 0
 vim.opt.cursorline = false
 
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 3
+
 vim.opt.whichwrap:append("<,>,h,l,[,]") 
